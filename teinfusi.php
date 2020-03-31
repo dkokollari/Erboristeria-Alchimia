f7375cbcfb41eedaee8252b0242257a96c4eadf3
dkokollari <dardi.i@hotmail.com>
2020-03-31 15:56:33 +0200

    Aggiunta visualizzazione immagine te e infusi. Controllo che l'immagine esiste in img/te_e_infusi altrimenti viene mostrata una immagine statica. Pertanto aggiornati i riferimenti alle immagini e risolto il problema della visualizzazione [closes #53, #59]

--- teinfusi.php
+++ teinfusi.php
@@ -88,7 +88,8 @@
             while ($row = mysqli_fetch_assoc($result)) {
               print (
                 '<div class="card collapsed">'."\n".
-                //' <img src="'.(($row["immagine_te_e_infusi"]) ? 'data:image/jpeg;base64,'.base64_encode($row["immagine_te_e_infusi"]) : 'img/null.jpg').'"/>'."\n".
+                //controllo se l'immagine esiste (importante che sia in formato jpg) e in caso la visualizzo, altrimenti mostro un immagine statica presente nella directory
+                ' <img src="img/te_e_infusi/'.(file_exists("img/te_e_infusi/".$row["id_te_e_infusi"].".jpg") ? $row["id_te_e_infusi"].'.jpg' : '0.jpg').'"/>'."\n". 
                 ' <h3>'.htmlentities($row["nome_te_e_infusi"], ENT_NOQUOTES).'</h3>'."\n".
                 '   <h4>Ingredienti</h4>'."\n".
                 '   <p>'.nl2p(htmlentities($row["ingredienti_te_e_infusi"], ENT_NOQUOTES)).'</p>'."\n".
