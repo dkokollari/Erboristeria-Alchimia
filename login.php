a3f26ab06ad8c21db60b8f4cb9245116eb472c72
Fouad <fouadmouad5@gmail.com>
2020-04-16 17:10:59 +0200

    In teoria prima versione script ok, da testare!

--- login.php
+++ login.php
@@ -2,22 +2,64 @@
 require_once("DBAccess.php");
 
 if(isset($POST['login'])) {
-  $conn = new DBAccess();
+  $conn = new DBAccess(); //TODO FA SCHIFO RIPETERE STE RIGHE IN OGNI PAGINA PHP: MODULARIZZARE!
   if(!$conn->openConnection()) {
     echo "<h1>Impossibile connettersi al database riprovare pi&ugrave; tardi<h1>";
-    exit;
+    exit();
   }
+
   /*Stabilita connessione al db*/
+  $minLengthPwd = 8;
+  $maxLengthPwd = 12;
   $pagina = file_get_contents('login.html');
-  $errore = '<h1 class="errori"> La'.'email'. 'o la' .'password'. 'inserite sono sbagliate</h1>';
+  $errore = "";
+  $logged = "";
   $email = $POST['username']; // the username is one's email for us!
   $password = $POST['password'];
 
-  if(!$conn->query("SELECT * FROM utenti
-     WHERE email_utente = '".$email."' and password_utente = '".$password."'") {
-    $pagina = str_replace("%ERR_LOGIN%", $errore, $pagina);
+  if(!isset($email) || !isset($password)) {
+    $errore = '<h1 class= "errori"><h1>'. 'Inserire sia una' .'email'. 'che una'.'password';
   }
 
-  session_start(); //TODO fare pagine es i miei te e infusi riempita coi dati dell'utente presi dal db!
+  else if(!filter_input(INPUT_POST, $email, FILTER_VALIDATE_EMAIL)
+    || (strlen($password) < $minLengthPwd || strlen($password) > $maxLengthPwd)) {
+    $errore = '<h1 class="errori"> La'.'email'. 'o la' .'password'. 'inserite non sono corrette</h1>';
+  }
+
+  else {
+    /*password e email inserite dall'utente: ora controllo che ci siano nel db*/
+    $query = "SELECT * FROM utenti WHERE email_utente = ?";
+    $stmt = mysqli_stmt_init($conn);
+    if(!mysqli_stmt_prepare($stmt, $query)) {
+      echo "Errore della query:" . mysqli_error($connessione) . ".";
+      exit();
+    }
+
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    mysqli_stmt_execute($stmt);
+    $result = mysqli_stmt_get_result($stmt);
+    if($result->mysqli_num_rows($result) == 0) {
+      $errore = '<h1 class="errori"> La'.'email'. 'o la' .'password'. 'inserite non sono corrette</h1>';
+    }
+    else {
+      $row = mysqli_fetch_assoc($result);
+      $passwordCheck = password_verify($password, $row['password_utente']);
+      if($passwordCheck == false) {
+        $errore = '<h1 class="errori"> La'.'email'. 'o la' .'password'. 'inserite non sono corrette</h1>';
+      }
+      else {
+        session_start(); //TODO decidere cosa possono vedere gli utenti registrati!
+        $logged = "SEI LOGGATO, GUAGLIONE!!!";
+        $_SESSION['email_utente'] =  $row['email_utente'];
+      }
+      mysqli_free_result($result);
+    }
+  }
+
+  $pagina = str_replace("%ERR_LOGIN%", $errore , $pagina);
+  $pagina = str_replace("%ERR_LOGIN%", $logged , $pagina);
+  echo $pagina;
+  $conn->closeConnection();
+
 }
 ?>
