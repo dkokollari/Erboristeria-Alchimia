a32f43ca13b0518e03d82ff9c38977ea9583f559
Fouad <fouadmouad5@gmail.com>
2020-04-21 02:05:08 +0200

    Versione base ricerca in php: check!

--- ricerca.php
+++ ricerca.php
@@ -13,7 +13,7 @@ if(isset($_POST['search'])) {
   /* visualizza il set di caratteri utilizzato dal client */
   //printf("Initial character set: %s\n", $con->character_set_name());
   /* cambia il set di caratteri in utf8 */
-  if (!$conn->set_charset("utf8")) {
+  if (!$conn->connection->set_charset("utf8")) {
       //printf("Error loading character set utf8: %s\n", $con->error);
       exit(1);
   }
@@ -23,13 +23,15 @@ if(isset($_POST['search'])) {
 
   $query_ricerca = "SELECT nome_articolo, quantita_magazzino_articolo
                     FROM articoli
-                    ORDER BY id_articolo DESC
-                    WHERE nome_articolo = ?";
+                    WHERE nome_articolo LIKE ?
+                    ORDER BY id_articolo DESC";
 
   if(!$stmt = mysqli_prepare($conn->connection, $query_ricerca)) {
-    die('prepare() failed: ' . htmlspecialchars(mysqli_error($conn->$connection)));
+    die('prepare() failed: ' . htmlspecialchars(mysqli_error($conn->connection)));
   }
-
+  
+  $search_value = preg_replace("#[^0-9a-z]#", "", $search_value); 
+  $search_value = '%'.$search_value.'%';
   $stmt->bind_param("s", $search_value);
   $stmt->execute();
   $result = $stmt->get_result();
@@ -38,12 +40,16 @@ if(isset($_POST['search'])) {
     $resultsToPrint = '<span>La ricerca non ha prodotto alcun risultato</span>';
   }
   else while($row = $result->fetch_assoc()) {
-    $resultsToPrint .= '<li>' .'nome: '. .$row['nome_articolo']. 'quantit&agrave;: ' .$row['nome_articolo']. '</li>';
+    $resultsToPrint .= '<li>' .'nome: ' .$row['nome_articolo']. 'quantit&agrave;: ' .$row['nome_articolo']. '</li>';
   }
 
   $stmt->close();
   $pagina = str_replace("%SEARCH_RESULTS%", $resultsToPrint , $pagina);
   echo $pagina;
   $conn->closeConnection();
+}
+else {
+  $pagina = str_replace("%SEARCH_RESULTS%", "" , $pagina);
+  echo $pagina;
 }
 ?>
