1ea08b62fcb3541923b77df306cb10f0038d9964
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-04-25 19:46:55 +0200

    Aggiunto riferimento a getImage()

--- teinfusi.php
+++ teinfusi.php
@@ -6,11 +6,12 @@
   if($con->openConnection()){
     $pagina = file_get_contents('teinfusi.html');
     $lista_te_e_infusi = $con->getTeInfusi();
+    $image = new Image();
 
     foreach ($lista_te_e_infusi as $row){
       $lista .=
       '<button class="card collapsed">
-        <img src="img/te_e_infusi/'.(file_exists("img/te_e_infusi/".$row["id_te_e_infusi"].".jpg") ? $row["id_te_e_infusi"].".jpg" : "0.jpg").'"/>
+        <img src="'.$image->getImage("./img/te_e_infusi/", $row["id_te_e_infusi"]).'"/>
         <h3><dt>'.$row["nome_te_e_infusi"].'</dt></h3>
         <dd>
           <h4>Ingredienti</h4>
