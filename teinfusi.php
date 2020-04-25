9f9a61d7d0a8ce7c6efefeda40cd11c34a793fb5
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-04-25 20:52:46 +0200

    Indentato il codice sorgente

--- teinfusi.php
+++ teinfusi.php
@@ -17,21 +17,21 @@
 
     foreach ($lista_te_e_infusi as $row){
       $lista .=
-      '<button class="card collapsed">
-        <img src="'.$image->getImage("./img/te_e_infusi/", $row["id_te_e_infusi"]).'"/>
-        <h3><dt>'.htmlentities($row["nome_te_e_infusi"]).'</dt></h3>
-        <dd>
-          <h4>Ingredienti</h4>
-          <p>'.nl2p(htmlentities($row["ingredienti_te_e_infusi"])).'</p>
-          <h4>Descrizione</h4>
-          <p>'.nl2p(htmlentities($row["descrizione_te_e_infusi"])).'</p>
-          <h4>Preparazione</h4>
-          <p>'.nl2p(htmlentities($row["preparazione_te_e_infusi"])).'</p>
-        </dd>
-        <i class="material-icons-round">expand_more</i>
-      </button>
+        '<button class="card collapsed">
+          <img src="'.$image->getImage("./img/te_e_infusi/", $row["id_te_e_infusi"]).'"/>
+          <h3><dt>'.htmlentities($row["nome_te_e_infusi"]).'</dt></h3>
+          <dd>
+            <h4>Ingredienti</h4>
+            <p>'.nl2p(htmlentities($row["ingredienti_te_e_infusi"])).'</p>
+            <h4>Descrizione</h4>
+            <p>'.nl2p(htmlentities($row["descrizione_te_e_infusi"])).'</p>
+            <h4>Preparazione</h4>
+            <p>'.nl2p(htmlentities($row["preparazione_te_e_infusi"])).'</p>
+          </dd>
+          <i class="material-icons-round">expand_more</i>
+        </button>
 
-      ';
+        ';
     }
 
     $pagina = str_replace("%LISTA_TE_E_INFUSI%", $lista, $pagina);
