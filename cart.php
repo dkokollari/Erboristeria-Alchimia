983523d51caf4631d1db5d7a45c164c1d94c806e
Fouad <fouadmouad5@gmail.com>
2020-05-21 18:11:29 +0200

    Fix indici $_SESSION[] dopo rimozione prodotti dal carrello

--- cart.php
+++ cart.php
@@ -37,11 +37,11 @@
 
   if(filter_input(INPUT_GET, 'action') == 'delete') {
     foreach($_SESSION['shopping_cart'] as $key => $product) {
-      if($product['id'] == filter_input(INPUT_GET, 'id')) {
+      if($product['id_articolo'] == filter_input(INPUT_GET, 'id_articolo')) {
         unset($_SESSION['shopping_cart'][$key]);
       }
     }
-    $_SESSION['shopping_cart'] = array_values([$_SESSION['shopping_cart']]);
+   $_SESSION['shopping_cart'] = array_values($_SESSION['shopping_cart']);
   }
 
   /*
