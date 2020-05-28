a292af0e1349725f3b2496b1ec0f72e2caae5488
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-28 20:51:09 +0200

    Indentato il codice dinamico

--- modifica_teInfusi.php
+++ modifica_teInfusi.php
@@ -23,21 +23,21 @@
       $descrizione = nl2p(htmlentities($row["descrizione_te_e_infusi"]));
       $preparazione = nl2p(htmlentities($row["preparazione_te_e_infusi"]));
       $lista .=
-      '<div class="card collapsed" title="'.$nome.'. Premi per espandere la descrizione">
-      <h3>'.$nome.'</h3>
-      <a class="accessibility_hidden">Salta la descrizione di questo t&egrave; o infuso</a>
-      <img src="'.$immagine.'" alt="'.$descrizione_immagine.'"/>
-      <h4>Ingredienti</h4>
-      <p>'.$ingredienti.'</p>
-      <h4>Descrizione</h4>
-      <p>'.$descrizione.'</p>
-      <h4>Preparazione</h4>
-      <p>'.$preparazione.'</p>
-      <a href="updateTeInfusi.php?id='.$id.'">Modifica</a>
-      <span class="expand_btn material-icons-round">expand_more</span>
-      </div>
+        '<div class="card collapsed" title="'.$nome.'. Premi per espandere la descrizione">
+        <h3>'.$nome.'</h3>
+        <a class="accessibility_hidden">Salta la descrizione di questo t&egrave; o infuso</a>
+        <img src="'.$immagine.'" alt="'.$descrizione_immagine.'"/>
+        <h4>Ingredienti</h4>
+        <p>'.$ingredienti.'</p>
+        <h4>Descrizione</h4>
+        <p>'.$descrizione.'</p>
+        <h4>Preparazione</h4>
+        <p>'.$preparazione.'</p>
+        <a href="updateTeInfusi.php?id='.$id.'">Modifica</a>
+        <span class="expand_btn material-icons-round">expand_more</span>
+        </div>
 
-      ';
+        ';
     }
 
     $pagina = str_replace("%LISTA_TE_E_INFUSI%", $lista, $pagina);
