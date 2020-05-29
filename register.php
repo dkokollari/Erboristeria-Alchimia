4f68f52adbd15060fc9ea667724fddba4fe6aacc
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-29 23:16:20 +0200

    Aggiunto cattura input da form

--- register.php
+++ register.php
@@ -1,3 +1,9 @@
 <?php
     require_once("DBAccess.php");
+
+    $pagina = file_get_contents('register.html');
+    if($_POST['Registrati']){
+      $email = mysql_real_escape_string(trim($_POST['email']));
+      $password = mysql_real_escape_string(trim($_POST['password']));
+    }
 ?>
