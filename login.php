ea5450dac96d6576203fc501a770ab9063677682
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-29 22:19:33 +0200

    Revert commit fe6b792

--- login.php
+++ login.php
@@ -15,8 +15,8 @@
   if($_POST['Login']){
     $email = mysql_real_escape_string(trim($_POST['email']));
     $password = mysql_real_escape_string(trim($_POST['password']));
-    const $minLengthPwd = 8;
-    const $maxLengthPwd = 12;
+    $minLengthPwd = 8;
+    $maxLengthPwd = 12;
     $errore = "";
     $logged = "";
     const $errore_empty = '<p class="errore">Inserire sia una email che una password</p>';
