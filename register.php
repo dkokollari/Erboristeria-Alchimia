0beb110b307242bd4f087085289db5541ad0481a
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-29 23:23:25 +0200

    Aggiunto messaggi di errore

--- register.php
+++ register.php
@@ -7,5 +7,9 @@
       $password = mysql_real_escape_string(trim($_POST['password']));
       $minLengthPwd = 8;
       $maxLengthPwd = 12;
+      $errore_empty = '<p class="errore">Inserisci una email e una password</p>';
+      $errore_full = '<p class="errore">Questo utente sembra essere gi&agrave; registrato</p>';
+      $errore_email = '<p class="errore">Inserisci una email valida</p>';
+      $errore_password = '<p class="errore">Inserisci una password tra gli 8 e i 12 caratteri</p>';
     }
 ?>
