c660568452dc5ea64f5d45afe8a02728e02c660d
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-29 12:40:55 +0200

    Abbellito il codice

--- deleleTeInfusi.php
+++ deleleTeInfusi.php
@@ -1,19 +1,24 @@
 <?php
-require_once("DBAccess.php");
-require_once("Image.php");
+  require_once("DBAccess.php");
+  require_once("Image.php");
 
-$con = new DBAccess();
-$img = new Image();
-$id = $_GET['id'];
+  $con = new DBAccess();
+  $img = new Image();
+  $id = $_GET['id'];
 
-if($con->openConnection()){
-  if($con->deleteTeInfusi_by_id($id)){
-  	$img->deleteImage("img/te_e_infusi/".$id."jpg");
-    echo "query esequita con successo";
-  }
-  else echo "query non esequita";
-} else { echo " Operazione non eseguita riprova dopo";}
+  if($con->openConnection()){
+    if($con->deleteTeInfusi_by_id($id)){
+  	   $img->deleteImage("img/te_e_infusi/".$id."jpg");
+       echo "query esequita con successo";
+     }
+     else{
+       echo "query non esequita";
+     }
+   }
+   else{
+     echo "Operazione non eseguita riprova dopo";
+   }
 
-header("Refresh:1; url=modifica_teInfusi.php");
-exit;
+  header("Refresh:1; url=modifica_teInfusi.php");
+  exit;
 ?>
