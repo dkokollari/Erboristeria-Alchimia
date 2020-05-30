54eb4e28b87ebffbc5dc095da3cdbd8ab8d8428b
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-30 13:48:38 +0200

    Aggiornato messaggio di errore e controllo su campi vuoti

--- register.php
+++ register.php
@@ -10,12 +10,16 @@
       $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
       $minLengthPwd = 8;
       $maxLengthPwd = 12;
-      $errore_empty = '<p class="errore">Inserisci una email e una password</p>';
+      $errore_empty = '<p class="errore">Completa tutti i campi</p>';
       $errore_full = '<p class="errore">Questo utente sembra essere gi&agrave; registrato</p>';
       $errore_email = '<p class="errore">Inserisci una email valida</p>';
       $errore_password = '<p class="errore">Inserisci una password tra gli 8 e i 12 caratteri</p>';
 
-      if(empty($email) || empty($password)){
+      if(empty($nome)
+      || empty($cognome)
+      || empty($email)
+      || empty($password)
+      || empty($data_nascita)){
         $errore = $errore_empty;
       }
       else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
