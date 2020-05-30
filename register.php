e53bd245322224578a4c02c3d372ec993527063d
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-30 14:40:33 +0200

    Aggiunto controllo lunghezza $nome, $cognome Aggiornati i relativi messaggi di errore

--- register.php
+++ register.php
@@ -9,13 +9,17 @@
       $password = mysql_real_escape_string(trim($_POST['password']));
       $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
       /* requisiti campi */
+      $minLengthUsr = 2;
+      $maxLengthUsr = 20;
       $minLengthPwd = 8;
       $maxLengthPwd = 12;
       /* messaggi di errore */
       $errore_empty = '<p class="errore">Completa tutti i campi</p>';
       $errore_full = '<p class="errore">Questo utente sembra essere gi&agrave; registrato</p>';
+      $errore_nome = '<p class="errore">Inserisci un nome tra '.$minLengthUsr.' e '.$maxLengthUsr.' caratteri</p>';
+      $errore_cognome = '<p class="errore">Inserisci un cognome tra '.$minLengthUsr.' e '.$maxLengthUsr.' caratteri</p>';
       $errore_email = '<p class="errore">Inserisci una email valida</p>';
-      $errore_password = '<p class="errore">Inserisci una password tra gli 8 e i 12 caratteri</p>';
+      $errore_password = '<p class="errore">Inserisci una password tra gli '.$minLengthPwd.' e i '.$maxLengthPwd.' caratteri</p>';
 
       if(empty($nome)
       || empty($cognome)
@@ -23,6 +27,12 @@
       || empty($password)
       || empty($data_nascita)){
         $errore = $errore_empty;
+      }
+      else if(strlen($nome) < $minLengthUsr || strlen($nome) > $maxLengthUsr){
+        $errore = $errore_nome;
+      }
+      else if(strlen($cognome) < $minLengthUsr || strlen($cognome) > $maxLengthUsr){
+        $errore = $errore_cognome;
       }
       else if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
         $errore = $errore_email;
