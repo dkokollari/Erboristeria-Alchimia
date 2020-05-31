c17279c374a4ba79a30e6ab05ad2c406b8f33f7d
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-05-31 15:19:11 +0200

    Corretto lunghezza massima stringa di check_str() Fare riferimento alla lunghezza definita dal campo nel database (in questo caso utenti.nome_utente, utenti.cognome_utente)

--- validate_form.php
+++ validate_form.php
@@ -1,15 +1,15 @@
 <?php
   /* lunghezza minima 8 e massima 12 caratteri, almeno 1 lettera ed 1 numero */
   function check_pwd($password){
-          if(preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/',$password)==1){
+          if(preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/', $password)==1){
               return true;
           }
           return false;
       }
 
-  /* lunghezza minima 3 e massima 255 caratteri */
+  /* lunghezza minima 3 e massima 200 caratteri */
   function check_str($stringa){
-          if(preg_match('^([A-Z] | [a-z])[a-z]{3,255}',$stringa)==1){
+          if(preg_match('^([A-Z] | [a-z])[a-z]{3,200}', $stringa)==1){
               return true;
           }
           return false;
