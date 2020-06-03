b639df23856bf385edbd84b973035cbb102b3727
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-03 14:09:00 +0200

    Spostato commenti di check_pwd(), check_str()

--- validate_form.php
+++ validate_form.php
@@ -1,12 +1,14 @@
 <?php
   class Validate_form{
-    /* lunghezza minima 8 e massima 12 caratteri, almeno 1 lettera ed 1 numero */
+
     public function check_pwd($password){
+      // lunghezza minima 8 e massima 12 caratteri, almeno 1 lettera ed 1 numero
       return preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/', $password);
     }
 
-    /* lunghezza minima 3 e massima 200 caratteri */
+
     public function check_str($stringa){
+      // lunghezza minima 3 e massima 200 caratteri
       return preg_match('/^([A-Z]|[a-z])[a-z]{2,255}$/', $stringa);
     }
 
