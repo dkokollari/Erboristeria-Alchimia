8298511fc7e1d2045bc089670e16e7693c2bc73a
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-03 14:31:39 +0200

    Modificato regex in check_str()

--- validate_form.php
+++ validate_form.php
@@ -1,15 +1,13 @@
 <?php
   class Validate_form{
-
     public function check_pwd($password){
       // lunghezza minima 8 e massima 12 caratteri, almeno 1 lettera ed 1 numero
       return preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/', $password);
     }
 
-
     public function check_str($stringa){
       // lunghezza minima 3 e massima 200 caratteri
-      return preg_match('/^([A-Z]|[a-z])[a-z]{2,255}$/', $stringa);
+      return preg_match('/^([A-Z]|[a-z]){3,255}$/', $stringa);
     }
 
     public function is_empty($params=null){
