9421bb87cedc01d5ac574a5fa2765726c5f852cc
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-03 13:55:10 +0200

    Aggiunto public su check_pdw(), check_str(), is_empty()

--- validate_form.php
+++ validate_form.php
@@ -1,16 +1,16 @@
 <?php
   class Validate_form{
     /* lunghezza minima 8 e massima 12 caratteri, almeno 1 lettera ed 1 numero */
-    function check_pwd($password){
+    public function check_pwd($password){
       return preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,12}$/', $password);
     }
 
     /* lunghezza minima 3 e massima 200 caratteri */
-    function check_str($stringa){
+    public function check_str($stringa){
       return preg_match('/^([A-Z]|[a-z])[a-z]{2,255}$/', $stringa);
     }
 
-    function is_empty($params=null){
+    public function is_empty($params=null){
       foreach ($params as $value){
         if(empty($value)) return true;
       }
