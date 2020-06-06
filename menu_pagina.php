0ad46bdae3ade097a3ecce7c2e18c23df6c6b12c
marcelwandji <marcelwandjijr@gmail.com>
2020-06-06 20:56:06 +0200

    fixed classe menu_pagina

--- menu_pagina.php
+++ menu_pagina.php
@@ -5,67 +5,77 @@ class menu_pagina{
 
   public static function menu($pagina)
   {
-    $menu ='';
-    if($pagina=="teinfusi.php"){
-      $menu = '<li><a href="homepage.html" xml:lang="en">Homepage</a></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li class="selected_page"><p>T&egrave; &amp; Infusi</p></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
-    }
-    else if($pagina=="homepage.php"){
-      $menu = '<li class="selected_page"><span xml:lang="en">Homepage</span></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
-    }
-    else if($pagina=="eventi.php"){
-      $menu = '<li><a href="homepage.html"><span xml:lang="en">Homepage</span></a></li>
-          <li class="selected_page">Eventi</li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
-    }
-    else if($pagina=="prodotti.php"){
-      $menu = '<li><a href="homepage.html"><span xml:lang="en">Homepage</span></a></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li class="selected_page">Prodotti</li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
-    }
-    else if($pagina=="informazioni.php"){
-      $menu = '<li><a href="homepage.html"><span xml:lang="en">Homepage</span></a></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li class="selected_page">Informazioni</li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
-    }
-    else if($pagina=="la_mia_storia.php"){
-      $menu = '<li><a href="homepage.html"><span xml:lang="en">Homepage</span></a></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li class="selected_page">La mia Storia</li>';
+    $menu ='<li><a href="home.php" xml:lang="en">home</a></li>
+              <li><a href="teinfusi.php">t&egrave; &amp; infusi</a></li>
+              <li><a href="prodotti.php">prodotti</a></li>
+              <li><a href="eventi.php">eventi</a></li>
+              <li><a href="la_mia_storia.php">la mia storia</a></li>
+              <li><a href="informazioni.php">informazioni</a></li>';
+
+    if(isset($_SESSION['auth']) && !$_SESSION['auth']){
+      $menu .= '<li><a href="login.php">accedi</a></li>';
     }
     else{
-      $menu = '<li><a href="homepage.html"><span xml:lang="en">Homepage</span></a></li>
-          <li><a href="eventi.html">Eventi</a></li>
-          <li><a href="teinfusi.php">T&egrave; &amp; Infusi</a></li>
-          <li><a href="prodotti.html">Prodotti</a></li>
-          <li><a href="informazioni.html">Informazioni</a></li>
-          <li><a href="la_mia_storia.html">La mia Storia</a></li>';
+
+      if($_SESSION['utente']=='User'){
+            $menu .= '<li><a href="carrello.php">carrello</a></li>';
+      }
+
+      $menu .= '<li><a href="profilo.php">profilo</a></li>
+                <li><a href="loggout.php" xml:lang="en">loggout</a></li>';
+    }
+
+    return menu_pagina::paginaCorrente($pagina, $menu);
+  }
+
+  private static function paginaCorrente($pagina, $menu){
+
+    if($pagina=="home.php"){
+      $menu = str_replace('<li><a href="home.php" xml:lang="en">home</a></li>',
+                    '<li class="selected_page"><span xml:lang="en">home</span></li>',$menu);
+    }
+
+    else if($pagina=="teinfusi.php"){
+      $menu = str_replace('<li><a href="teinfusi.php">t&egrave; &amp; infusi</a></li>',
+                    '<li class="selected_page">t&egrave; &amp; infusi</li>',$menu);
+    }
+
+    else if($pagina=="prodotti.php"){
+      $menu = str_replace('<li><a href="prodotti.php">prodotti</a></li>',
+                    '<li class="selected_page">prodotti</li>',$menu);
+    }
+
+    else if($pagina=="eventi.php"){
+      $menu = str_replace('<li><a href="eventi.php">eventi</a></li>',
+                    '<li class="selected_page">eventi</li>',$menu);
+    }
+
+    else if($pagina=="la_mia_storia.php"){
+      $menu = str_replace('<li><a href="la_mia_storia.php">la mia storia</a></li>',
+                    '<li class="selected_page">la mia storia</li>',$menu);
+    }
+
+    else if($pagina=="informazioni.php"){
+      $menu = str_replace('<li><a href="informazioni.php">informazioni</a></li>',
+                    '<li class="selected_page">informazioni</li>',$menu);
+    }
+
+    else if($pagina=="carrello.php"){
+      $menu = str_replace('<li><a href="carrello.php">carrello</a></li>',
+                    '<li class="selected_page">carrello</li>',$menu);
+    }
+
+    else if($pagina=="profilo.php"){
+      $menu = str_replace('<li><a href="profilo.php">profilo</a></li>',
+                    '<li class="selected_page">profilo</li>',$menu);
     }
 
     return $menu;
 
   }
+
+
+
 }
 
 
