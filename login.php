e840dc0c740f90a411085d054e03a2d5b5226bb0
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-11 17:25:12 +0200

    Abbellito il codice

--- login.php
+++ login.php
@@ -12,7 +12,7 @@
   $pagina = file_get_contents('login.html');
   $email = '';
   $password = '';
-  if($_POST['Login']){
+  if($_POST['Login']) {
     $email = mysql_real_escape_string(trim($_POST['email']));
     $password = mysql_real_escape_string(trim($_POST['password']));
     $minLengthPwd = 8;
@@ -21,29 +21,26 @@
     $logged = "";
     $errore_empty = '<span class="errore">Inserire sia una email che una password</span>';
     $errore_wrong = '<span class="errore">La email o la password inserite non sono corrette</span>';
-    if(empty($email) || empty($password)){
-      $errore = $errore_empty;
-    }
-
-    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)
-    || (strlen($password) < $minLengthPwd || strlen($password) > $maxLengthPwd)){
+    if(empty($email) || empty($password)) $errore = $errore_empty;
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL) ||
+               (strlen($password) < $minLengthPwd || strlen($password) > $maxLengthPwd)) {
       $errore = $errore_wrong;
     }
-    else{
+    else {
       /*password e email inserite dall'utente: ora controllo che ci siano nel db*/
       $conn = new DBAccess();
-      if(!$conn->openConnection()){
+      if(!$conn->openConnection()) {
        echo '<span class="errore">Impossibile connettersi al database riprovare pi&ugrave; tardi</span>';
        exit;
       }
       //Stabilita connessione al db
       $query = "SELECT * FROM `utenti` WHERE `email_utente`=?";
-  	  if(!$conn->connection->set_charset("utf8")){
+      if(!$conn->connection->set_charset("utf8")) {
         echo '<span class="errore">Error: Unable to set the character set!</span>';
         exit;
     	}
 
-      if(!$stmt = mysqli_prepare($conn->connection, $query)){
+      if(!$stmt = mysqli_prepare($conn->connection, $query)) {
         die('prepare() failed: '.htmlspecialchars(mysqli_error($conn->$connection)));
       }
 
@@ -51,18 +48,13 @@
       $stmt->execute();
       $result = $stmt->get_result();
 
-      if($result->num_rows === 0){
-      $errore = $errore_wrong;
-      }
-      else{
+      if($result->num_rows === 0) $errore = $errore_wrong;
+      else {
         $row = $result->fetch_assoc();
         $passwordCheck = password_verify($password, $row['password_utente']);
-        /*inserimento solo tramite php cosi settiamo bcrypt come algoritmo!*/
-        if($passwordCheck == false){
-          $errore = $errore_wrong;
-        }
-        else{
-          if(isset($_POST['remember_me'])){
+        if($passwordCheck == false) $errore = $errore_wrong;
+        else {
+          if(isset($_POST['remember_me'])) {
             setcookie("email",$email,time()+60*60*24*30);
             setcookie("password",$password,time()+60*60*24*30);
           }
@@ -78,9 +70,8 @@
     $pagina = str_replace("%ERR_LOGIN%", $errore, $pagina);
     $pagina = str_replace("%LOGIN_STATUS%", $logged, $pagina);
     echo $pagina;
-  }
-  /*utente non ha premuto il tasto submit*/
-  else{
+  } // end if $_POST['Login']
+  else {
     $pagina = str_replace("%ERR_LOGIN%", "", $pagina);
     $pagina = str_replace("%LOGIN_STATUS%", "", $pagina);
     echo $pagina;
