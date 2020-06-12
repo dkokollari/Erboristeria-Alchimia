18094c34f31022f7b573c5aeab067fb262929e09
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-12 19:47:39 +0200

    Spostato return statement

--- Errore.php
+++ Errore.php
@@ -1,24 +1,21 @@
 <?php
-class Errore {
+  class Errore {
+    public static function getErrore($err) {
+      $msg='';
+      switch($err) {
+        case '1' :
+           $msg = 'Impossibile connettersi al <span xml:lang="en">database</span>, riprovare pi&ugrave; tardi';
+           break;
 
-  public static function getErrore($err) {
+       case '2' :
+          $msg = '404 : pagina non trovata';
+          break;
 
-    $msg='';
-    switch($err) {
-      case '1' :
-         $msg = 'Impossibile connettersi al <span xml:lang="en">database</span>, riprovare pi&ugrave; tardi';
-         break;
-
-     case '2' :
-        $msg = '404 : pagina non trovata';
-        break;
-
-      case '3' :
-        $msg = '403 : la risorsa a cui cerca di accedere non &egrave; disponibile';
-        break;
-
-     return $msg; 
+        case '3' :
+          $msg = '403 : la risorsa a cui cerca di accedere non &egrave; disponibile';
+          break;
+      }
+     return $msg;
     }
   }
-}
- ?>
+?>
