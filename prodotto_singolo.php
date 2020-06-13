87e47b0d136555b1b20701e1c8d7d41db51929af
Fouad <fouadmouad5@gmail.com>
2020-06-13 11:43:30 +0200

    Inizio pagina php prodotto singolo

---
+++ prodotto_singolo.php
@@ -0,0 +1,8 @@
+<?php
+  require_once("DBAccess.php");
+  $conn = new DBAccess();
+  if(!$conn->openConnection()) {
+   echo '<p class= "errori">' . "Impossibile connettersi al database: riprovare pi&ugrave; tardi" . '</p>';
+   exit(1);
+  }
+?>
