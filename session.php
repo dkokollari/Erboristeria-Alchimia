fa4f0a371f1e4e9dc39c71e51514e241bf131f35
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-13 18:08:53 +0200

    Aggiunto riga vuota

--- session.php
+++ session.php
@@ -2,6 +2,7 @@
     session_start();
 
     if(!isset($_SESSION['auth'])) $_SESSION['auth'] = false;
+    
     if(!isset($_SESSION['tipo_utente'])) $_SESSION['tipo_utente'] = 'Visitatore';
 
     if(!isset($_SESSION['nome_utente'])) $_SESSION['nome_utente']="";
