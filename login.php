f1e310c2f611bd6d3ced120946dcfa014fde7859
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-13 17:48:50 +0200

    Merge login.php da branch feature/profilo_utente

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
   //require_once("sessione.php");
   session_start();
 
-  if(isset($_SESSION['email_utente']) && $_SESSION['email_utente']="") { // utente con sessione
+  if(isset($_SESSION['email_utente']) && $_SESSION['email_utente']!="") { // utente con sessione
     header('location:index.php');
     exit;
   }
@@ -15,23 +15,20 @@
     }
     else {
       $temp = $aux->getUser($_COOKIE['email']);
-      if(empty(temp)) { // utente non trovato TODO: allora eliminare cookie
-
+      if(empty($temp) ||
+          password_verify($_COOKIE['password'], $temp[0]['password_utente'])) { // utente non trovato o password diversa
+        setcookie("email", "", time()-3600);
+        setcookie("password", "", time()-3600);
       }
       else {
-        $_SESSION['nome_utente'] = $temp[0]['nome_utente'];
-        $_SESSION['cognome_utente'] = $temp[0]['cognome_utente'];
-        $_SESSION['email_utente'] = $temp[0]['email_utente'];
-        $_SESSION['password_utente'] = $temp[0]['password_utente'];
-        $_SESSION['tipo_utente'] = $temp[0]['tipo_utente'];
-        $_SESSION['data_nascita_utente'] = $temp[0]['data_nascita_utente'];
+        setSessione($temp);
         header('location:index.php');
         exit;
       }
     }
   } // end else if utente con cookie senza sessione
 
-  // utente senza cookie senza sessione
+  // utente senza cookie e senza sessione
   $pagina = file_get_contents("login.html");
   if($_POST['Login']) {
     $email = mysql_real_escape_string(trim($_POST['email']));
@@ -48,7 +45,7 @@
     else { // email e password valide
       $con = new DBAccess();
       if(!$con->openConnection()) {
-        header('Location: redirect.php?error=1');
+        echo '<span class="errore">Impossibile connettersi al database riprovare pi&ugrave; tardi</span>';
         exit;
       }
       else {
@@ -58,12 +55,7 @@
           $passwordCheck = password_verify($password, $utente[0]['password_utente']);
           if(!$passwordCheck) $errore = $errore_wrong;
           else {
-            $_SESSION['nome_utente'] = $utente[0]['nome_utente'];
-            $_SESSION['cognome_utente'] = $utente[0]['cognome_utente'];
-            $_SESSION['email_utente'] = $utente[0]['email_utente'];
-            $_SESSION['password_utente'] = $utente[0]['password_utente'];
-            $_SESSION['tipo_utente'] = $utente[0]['tipo_utente'];
-            $_SESSION['data_nascita_utente'] = $utente[0]['data_nascita_utente'];
+            setSessione($utente);
             if(isset($_POST['remember_me'])) {
               setcookie('email', $email, time()+60*60*24*30);
               setcookie('password', $password, time()+60*60*24*30);
@@ -79,4 +71,13 @@
   $pagina = str_replace("%ERR_LOGIN%", $errore, $pagina);
   $pagina = str_replace("%LOGIN_STATUS%", $logged, $pagina);
   echo $pagina;
+
+  function setSessione($array="") { // guardare DBAccess::getUser() per vedere la struttura di $array
+    $_SESSION['nome_utente'] = $array[0]['nome_utente'];
+    $_SESSION['cognome_utente'] = $array[0]['cognome_utente'];
+    $_SESSION['email_utente'] = $array[0]['email_utente'];
+    $_SESSION['password_utente'] = $array[0]['password_utente'];
+    $_SESSION['tipo_utente'] = $array[0]['tipo_utente'];
+    $_SESSION['data_nascita_utente'] = $array[0]['data_nascita_utente'];
+  }
 ?>
