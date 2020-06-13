8bd63943c2476a28e5fae07400a3691d1ed1a38d
Fouad <fouadmouad5@gmail.com>
2020-06-14 01:33:54 +0200

    Finita scrittura pagina prosotto_singolo

--- prodotto_singolo.php
+++ prodotto_singolo.php
@@ -12,13 +12,6 @@
     }
 
     $pagina = file_get_contents('prodotto_singolo.html');
-    $meta_description = '';
-    $meta_keywords = '';
-    $img_1 = '';
-    $img_2 = '';
-    $desc_breve = '';
-    $desc_completa = '';
-    $ingredienti = '';
     $row = '';
     $iscriviti = '<p>Per poter acquistare online il seguente prodotto, se sei già registrato sul sito, fai il <a href="login.php">login</a>;
     altrimenti, che aspetti? <a href="register.php">Registrati</a> sul nostro sito: i clienti pi&ugrave; fedeli hanno una carta fedeltà che viene
@@ -40,8 +33,13 @@
     $meta_keywords = '<meta name="keywords" content="' . str_replace('', ', ', $row['nome_articolo']) . ' alt=' . $row['nome_articolo']  . '"/>';
     $img_1 = '<img id="immagine_prodotto" src="' . Image::getImage("../img/products/", $row['id_articolo']) . ' alt=' . $row['nome_articolo']  .  '"/>';
     $img_2 = '<img id="topbar_image" src="' . Image::getImage("../img/products/", $row['id_articolo']) . '"/>';
+    $nome = $row['nome_articolo'];
+    $prezzo = $row['prezzo_articolo'];
+    $linea = $row['linea_articolo'];
+    $categoria = $row['nome_categoria'];
     $desc_breve = '<p>' . $row['descrizione_articolo'] . '</p>'; //// WARNING: cè bisogno di un attributo 'descrizione breve' a db!
     $desc_completa = '<p>' . $row['descrizione_articolo'] . '</p>';
+    $preparazione =  '<p>' . $row['descrizione_articolo'] . '</p>'; /// WARNING: cè bisogno di un attributo 'preparazione' a db!
     $ingredienti =  '<p>' . $row['descrizione_articolo'] . '</p>'; /// WARNING: cè bisogno di un attributo 'preparazione' a db!
 
     if($_SESSION['utente'] == "Visitatore") {
@@ -51,10 +49,14 @@
     $pagina = str_replace('%META_KEYWORDS%' , $meta_keywords , $pagina);
     $pagina = str_replace('%PRODUCT_IMG_1%' , $img_1 , $pagina);
     $pagina = str_replace('%PRODUCT_IMG_2%' , $img_2 , $pagina);
+    $pagina = str_replace('%META_DESCRIPTION%' , $nome , $pagina);
+    $pagina = str_replace('%META_KEYWORDS%' , $prezzo , $pagina);
+    $pagina = str_replace('%PRODUCT_IMG_1%' , $linea , $pagina);
+    $pagina = str_replace('%PRODUCT_IMG_2%' , $categoria , $pagina);
     $pagina = str_replace('%SHORT_DESCRIPTION%' , $desc_breve , $pagina);
     $pagina = str_replace('%LONG_DESCRIPTION%' , $desc_completa , $pagina);
-    $pagina = str_replace('%HOW_TO%' , $ingredienti , $pagina);
-    $pagina = str_replace('%INGREDIENTS%' , $img_2 , $pagina);
+    $pagina = str_replace('%HOW_TO%' , $preparazione , $pagina);
+    $pagina = str_replace('%INGREDIENTS%' , $ingredienti , $pagina);
     echo $pagina;
 }
 ?>
