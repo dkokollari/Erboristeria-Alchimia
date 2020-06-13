b48b2c01a74e0bd15f43bf3c58bc1382c445dbd0
Fouad <fouadmouad5@gmail.com>
2020-06-13 21:20:38 +0200

    preparazione scheletro script generazione pagina prodotto_singolo

--- prodotto_singolo.php
+++ prodotto_singolo.php
@@ -1,5 +1,7 @@
 <?php
+  require_once("session.php");
   require_once("DBAccess.php");
+  require_once("Image.php");
   $conn = new DBAccess();
   if(!$conn->openConnection()) {
    echo '<p class= "errori">' . "Impossibile connettersi al database: riprovare pi&ugrave; tardi" . '</p>';
@@ -7,4 +9,17 @@
   }
 
   $pagina = file_get_contents('prodotto_singolo.html');
+    $meta_description = '';
+    $img_1='';
+    $img_2='';
+    $desc_breve = '';
+    $desc_completa = '';
+    $ingredienti = '';
+  $stmt = mysqli_prepare($conn->connection, $query_ricerca)
+
+  if($_SESSION['utente'] == "utente_registrato") {
+      //gestione carrello (bestemmie)
+  } else {
+      //sostituire tasto "aggiungi" con "<p>Crea un profilo per poter acquistare i nostri prodotti!</p>"
+  }
 ?>
