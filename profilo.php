1b649f109f87a0ed1defb159bf4717b3555f73b5
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-14 12:32:37 +0200

    Aggiunto sostituzione elementi registrazione in <head>

--- profilo.php
+++ profilo.php
@@ -8,16 +8,17 @@
   $_SESSION['data_nascita_utente'] = "";
 
   if($_SESSION['auth']) {
-    // sostituzione elementi di registrazione
     $pagina = file_get_contents('register.html');
+    // sostituzione elementi di registrazione
+    $pagina = str_replace('<title>Registrati - Erboristeria Alchimia</title>', '<title>Modifica profilo - Erboristeria Alchimia</title>', $pagina);
+    $pagina = str_replace('<meta name="title" content="Registrati ad Erboristeria Alchimia"/>', '<meta name="title" content="Modifica il profilo di Erboristeria Alchimia"/>', $pagina);
+    $pagina = str_replace('<meta name="description" content="Pagina di registrazione al sito"/>', '<meta name="description" content="Pagina di modifica del profilo"/>', $pagina);
+    $pagina = str_replace('<meta name="keywords" content="registrazione, email, password, erboristeria, alchimia"/>', '<meta name="keywords" content="modifica, aggiorna, profilo, profili, nome, cognome, username, e-mail, mail, password, data, erboristeria, alchimia"/>', $pagina);
     $pagina = str_replace('%REGISTER_STATUS%', '%STATUS_PROFILE%', $pagina);
-    $pagina = str_replace('<form action="register.php" method="post">',
-                          '<form action="profilo.php" method="post">', $pagina);
-    $pagina = str_replace('<h2>Registrati</h2>',
-                          '<h2>Modifica profilo</h2>', $pagina);
+    $pagina = str_replace('<form action="register.php" method="post">', '<form action="profilo.php" method="post">', $pagina);
+    $pagina = str_replace('<h2>Registrati</h2>', '<h2>Modifica profilo</h2>', $pagina);
     $pagina = str_replace('%REGISTER_ERROR%', '%ERROR_PROFILE%', $pagina);
-    $pagina = str_replace('<input id="log_in" type="submit" name="Registrati"/>',
-                          '<input id="log_in" type="submit" name="Modifica_profilo"/>', $pagina);
+    $pagina = str_replace('<input id="log_in" type="submit" name="Registrati"/>', '<input id="log_in" type="submit" name="Modifica_profilo"/>', $pagina);
     // pre-fill campi input
     $array_place_html = ['nome', 'cognome', 'username'];
     $array_place_session = ['nome_utente', 'cognome_utente', 'email_utente'];
