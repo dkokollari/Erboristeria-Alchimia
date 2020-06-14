870d5b8094ccaa8a02e8beb8c251f1eba947f86b
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-14 13:35:24 +0200

    Aggiunto controllo campi

--- profilo.php
+++ profilo.php
@@ -2,7 +2,7 @@
   // require_once("session.php");
   require_once("DBAccess.php");
   require_once("validate_form.php");
-  
+
   session_start();
   $_SESSION['auth'] = true;
   $_SESSION['nome_utente'] = "Mario";
@@ -33,7 +33,47 @@
     }
 
     if($_POST['Modifica_profilo']) {
+      $nome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['nome']))));
+      $cognome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['cognome']))));
+      $email = mysql_real_escape_string(trim($_POST['username']));
+      $password = mysql_real_escape_string(trim($_POST['password']));
+      $password_conferma = mysql_real_escape_string(trim($_POST['password_conferma']));
+      $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
+      /* messaggi di errore */
+      $errore_empty = '<span class="errore">Completa tutti i campi</span>';
+      $errore_nome = '<span class="errore">Inserisci un nome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+      $errore_cognome = '<span class="errore">Inserisci un cognome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+      $errore_email = '<span class="errore">Inserisci una email valida</span>';
+      $errore_password = '<span class="errore">Inserisci una password di lunghezza tra 8 e 100 caratteri, almeno 1 lettera ed 1 numero</span>'; // riferirsi alle regole di validate_form
+      $errore_conferma = '<span class="errore">Le password inserite non corrispondono</span>';
 
+      $fields = [$nome, $cognome, $email, $password, $data_nascita];
+      if(Validate_form::is_empty($fields)) {
+        $errore = $errore_empty;
+      }
+      else if(!Validate_form::check_str($nome)) {
+        $errore = $errore_nome;
+      }
+      else if(!Validate_form::check_str($cognome)) {
+        $errore = $errore_cognome;
+      }
+      else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errore = $errore_email;
+      }
+      else if(!Validate_form::check_pwd($password)) {
+        $errore = $errore_password;
+      }
+      else if($password != $password_conferma) {
+        $errore = $errore_conferma;
+      }
+      else {
+        $con = new DBAccess();
+        if(!$con->openConnection()) {
+         echo '<span class="errore">Impossibile connettersi al database riprovare pi&ugrave; tardi</span>';
+         exit;
+        }
+        // chiamata a funzione di inserimento utente
+        $con->closeConnection();
     }
 
     $pagina = str_replace('%STATUS_PROFILE%', '', $pagina);
