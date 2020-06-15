ec628e0b521f2c52aa159f6a2a6055d16348d8fd
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-15 12:35:47 +0200

    Aggiunto riferimento a genera()

--- prodotti.php
+++ prodotti.php
@@ -1,16 +1,7 @@
 <?php
-  require_once('session.php');
-  require_once('menu_pagina.php');
+  require_once("genera_pagine.php");
 
-  $pagina = file_get_contents('base.html');
-  $pagina = str_replace("%TITOLO_PAGINA%", "Prodotti", $pagina);
-  $pagina = str_replace("%DESCRIZIONE_PAGINA%", 'I prodotti online di Erboristeria Alchimia. Qualità, sicurezza e convenienza garantiti', $pagina);
-  $pagina = str_replace("%KEYWORDS_PAGINA%", 'prodotto, prodotti, cosmetici, alimentari, erboristeria, alchimia', $pagina);
-  $pagina = str_replace("%CONTAINER_PAGINA%", "container_prodotti", $pagina);
-  $pagina = str_replace("%LISTA_MENU%", menu_pagina::menu("prodotti.php"), $pagina);
-  $pagina = str_replace("%CONTENUTO_PAGINA%", file_get_contents('prodotti.html'), $pagina);
-  $pagina = ($_SESSION['auth'] && $_SESSION['tipo_utente']=="User"
-            ? str_replace("%ICONA_CARRELLO%", '<span id="cart_icon" class="material-icons-outlined top_icon">shopping_cart</span>', $pagina)
-            : str_replace("%ICONA_CARRELLO%", '', $pagina));
+  $contenuto = file_get_contents('prodotti.html');
+  $pagina = Genera_pagina::genera("base.html", "prodotti", $contenuto);
   echo $pagina;
 ?>
