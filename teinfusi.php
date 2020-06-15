daf0e467c54f76a98ec9c5e1b760e4a8b4e5df47
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-15 10:49:33 +0200

    Aggiunto riferimento a Genera_pagina::genera()

--- teinfusi.php
+++ teinfusi.php
@@ -4,20 +4,10 @@
   require_once("session.php");
   require_once("DBAccess.php");
   require_once("Image.php");
-  require_once('menu_pagina.php');
+  require_once("genera_pagina.php");
 
   $con = new DBAccess();
   if($con->openConnection()) {
-    $pagina = file_get_contents('base.html');
-    $pagina = str_replace("%TITOLO_PAGINA%", "T&egrave; &amp; Infusi", $pagina);
-    $pagina = str_replace("%DESCRIZIONE_PAGINA%", "Visualizza i nostri te e infusi", $pagina);
-    $pagina = str_replace("%KEYWORDS_PAGINA%", "te, infusi, te e infusi, erboristeria, alchimia", $pagina);
-    $pagina = str_replace("%CONTAINER_PAGINA%", "container_te_e_infusi", $pagina);
-    $pagina = str_replace("%LISTA_MENU%", menu_pagina::menu("teinfusi.php"), $pagina);
-    $pagina = ($_SESSION['auth'] && $_SESSION['tipo_utente']=="User"
-              ? str_replace("%ICONA_CARRELLO%", '<span id="cart_icon" class="material-icons-outlined top_icon">shopping_cart</span>', $pagina)
-              : str_replace("%ICONA_CARRELLO%", '', $pagina));
-
     $lista_te_e_infusi = $con->getTeInfusi();
 
     foreach ($lista_te_e_infusi as $row) {
@@ -49,12 +39,12 @@
         ';
     }
 
-    $container = file_get_contents('teinfusi.html');
-    $container = ($_SESSION['auth'] && $_SESSION['tipo_utente']=="Admin"
-                  ? str_replace("%NEW_TE_O_INFUSI%", '<a href="form_teinfusi.php">Aggiungi un nuovo T&egrave; o Infuso</a>', $container)
-                  : str_replace("%NEW_TE_O_INFUSI%", '', $container));
-    $container = str_replace("%LISTA_TE_E_INFUSI%", $lista, $container);
-    $pagina = str_replace("%CONTENUTO_PAGINA%", $container, $pagina);
+    $contenuto = file_get_contents('teinfusi.html');
+    $contenuto = ($_SESSION['auth'] && $_SESSION['tipo_utente']=="Admin"
+                  ? str_replace("%NEW_TE_O_INFUSI%", '<a href="form_teinfusi.php">Aggiungi un nuovo T&egrave; o Infuso</a>', $contenuto)
+                  : str_replace("%NEW_TE_O_INFUSI%", '', $contenuto));
+    $contenuto = str_replace("%LISTA_TE_E_INFUSI%", $lista, $contenuto);
+    $pagina = Genera_pagina::genera("base.html", "teinfusi", $contenuto);
     echo $pagina;
   }
   else {
