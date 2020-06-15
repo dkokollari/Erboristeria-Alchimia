c56b5371e26d99ac67c50f7abd4dc5c07d49a8b6
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-15 12:14:02 +0200

    Aggiunto riferimento a genera()

--- informazioni.php
+++ informazioni.php
@@ -1,16 +1,7 @@
 <?php
-  require_once('session.php');
-  require_once('menu_pagina.php');
+  require_once("genera_pagina.php");
 
-  $pagina = file_get_contents('base.html');
-  $pagina = str_replace("%TITOLO_PAGINA%", "Informazioni", $pagina);
-  $pagina = str_replace("%DESCRIZIONE_PAGINA%", 'Informazioni utili per contattarci: qui trovi i nostri contatti, gli orari di apertura e il nostro indirizzo', $pagina);
-  $pagina = str_replace("%KEYWORDS_PAGINA%", 'informazioni, orari, apertura, chiusura, email, mail, telefono, cellulare, posizione, mappa, erboristeria, alchimia', $pagina);
-  $pagina = str_replace("%CONTAINER_PAGINA%", "container_informazioni", $pagina);
-  $pagina = str_replace("%LISTA_MENU%", menu_pagina::menu("informazioni.php"), $pagina);
-  $pagina = $_SESSION['tipo_utente']=='User' ? str_replace("%ICONA_CARRELLO%",
-                    '<span id="cart_icon" class="material-icons-outlined top_icon">shopping_cart</span>', $pagina)
-                    : str_replace("%ICONA_CARRELLO%", '', $pagina);
-  $pagina = str_replace("%CONTENUTO_PAGINA%", file_get_contents('informazioni.html'), $pagina);
+  $contenuto = file_get_contents('informazioni.html');
+  $pagina = $pagina = Genera_pagina::genera("base.html", "informazioni", $contenuto);
   echo $pagina;
 ?>
