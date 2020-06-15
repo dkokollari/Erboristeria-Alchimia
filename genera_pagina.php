47431baa96d02eeb1cabead22c576f69edefd2ec
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-15 10:48:19 +0200

    Aggiunto generazione del contenuto (passato per parametro)

--- genera_pagina.php
+++ genera_pagina.php
@@ -3,7 +3,7 @@
 
   class Genera_pagina {
 
-    public function genera($base, $target) {
+    public function genera($base, $target, $contenuto="") {
     $pagina = file_get_contents($base);
     switch($target) {
       case "registrazione" :
@@ -28,6 +28,7 @@
     $pagina = str_replace("%CONTAINER_PAGINA%", $container_pagina, $pagina);
     $pagina = str_replace("%LISTA_MENU%", $lista_menu, $pagina);
     $pagina = str_replace("%ICONA_CARRELLO%", $icona_carrello, $pagina);
+    $pagina = str_replace("%CONTENUTO_PAGINA%", $contenuto, $pagina);
     return $pagina;
     }
 
