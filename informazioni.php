ec228eb06921b529f5ac1d2a6ed0a5bb401b84a8
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-15 12:36:13 +0200

    Rimosso assegnazione scorretta di $pagina

--- informazioni.php
+++ informazioni.php
@@ -2,6 +2,6 @@
   require_once("genera_pagina.php");
 
   $contenuto = file_get_contents('informazioni.html');
-  $pagina = $pagina = Genera_pagina::genera("base.html", "informazioni", $contenuto);
+  $pagina = Genera_pagina::genera("base.html", "informazioni", $contenuto);
   echo $pagina;
 ?>
