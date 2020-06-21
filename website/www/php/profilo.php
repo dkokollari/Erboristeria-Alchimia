c468f150af779ab251229c3ee1670ef81694f66b
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 21:35:50 +0200

    Aggiunto controllo $_POST['Modifica_profilo'])

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -2,7 +2,8 @@
   require_once("session.php");
   require_once("DBAccess.php");
   require_once("genera_pagina.php");
-
+  require_once("validate_form.php");
+  
   if($_SESSION['auth']) {
     $con = new DBAccess();
     if(!$con->openConnection()) {
@@ -37,6 +38,74 @@
                       </p>';
       }
     }
+
+    if($_POST['Modifica_profilo']) {
+      $nome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['nome']))));
+      $cognome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['cognome']))));
+      $email = mysql_real_escape_string(trim($_POST['username']));
+      $password = mysql_real_escape_string(trim($_POST['password']));
+      $password_conferma = mysql_real_escape_string(trim($_POST['password_conferma']));
+      $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
+      /* messaggi di errore */
+      $errore_empty = '<span class="errore">Completa tutti i campi</span>';
+      $errore_full = '<span class="errore">Questa e-mail sembra non essere disponibile</span>';
+      $errore_nome = '<span class="errore">Inserisci un nome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+      $errore_cognome = '<span class="errore">Inserisci un cognome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+      $errore_email = '<span class="errore">Inserisci una email valida</span>';
+      $errore_password = '<span class="errore">Inserisci una password di lunghezza tra 8 e 100 caratteri, almeno 1 lettera ed 1 numero</span>'; // riferirsi alle regole di validate_form
+      $errore_conferma = '<span class="errore">Le password inserite non corrispondono</span>';
+      $errore_sconosciuto = '<span class="errore">Per favore disconnettiti e accedi di nuovo</span>';
+
+      $fields = [$nome, $cognome, $email, $password, $data_nascita];
+      if(Validate_form::is_empty($fields)) {
+        $errore = $errore_empty;
+      }
+      else if(!Validate_form::check_str($nome)) {
+        $errore = $errore_nome;
+      }
+      else if(!Validate_form::check_str($cognome)) {
+        $errore = $errore_cognome;
+      }
+      else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        $errore = $errore_email;
+      }
+      else if(!Validate_form::check_pwd($password)) {
+        $errore = $errore_password;
+      }
+      else if($password != $password_conferma) {
+        $errore = $errore_conferma;
+      }
+      else {
+        $con = new DBAccess();
+        if(!$con->openConnection()) {
+          header('Location: redirect.php?error=1');
+          exit;
+        }
+        if($email != $_SESSION['email_utente'] && $con->getSingolo_Utenti($email)) {
+          $errore = $errore_full;
+        }
+        else {
+          $result = $con->updateUtenti($_SESSION['email_utente'], $nome, $cognome, $email, $password, $data_nascita);
+          if(!$result) {
+            $errore = $errore_sconosciuto;
+          }
+          else {
+            $array = $con->getSingolo_Utenti($email);
+            $_SESSION['nome_utente'] = $array[0]['nome_utente'];
+            $_SESSION['cognome_utente'] = $array[0]['cognome_utente'];
+            $_SESSION['email_utente'] = $array[0]['email_utente'];
+            $_SESSION['password_utente'] = $array[0]['password_utente'];
+            $_SESSION['tipo_utente'] = $array[0]['tipo_utente'];
+            $_SESSION['data_nascita_utente'] = $array[0]['data_nascita_utente'];
+          }
+        }
+        $con->closeConnection();
+      }
+
+      $status = (empty($errore)
+                ? "<span>Profilo aggiornato con successo</span>"
+                : $errore);
+    } // end if $_POST['Modifica_profilo']
 
     $contenuto = file_get_contents("../html/profilo.html");
     $contenuto = str_replace("%AGG_TIMBRI%", $aggTimbri, $contenuto);
