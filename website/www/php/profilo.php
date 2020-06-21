321c04275c9c9e2d6886730425d16e250a65a170
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 20:02:29 +0200

    Formattato i messaggi

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -18,21 +18,23 @@
       }
       $compleanno = DateTime::createFromFormat("Y-m-d", $_SESSION['data_nascita_utente']);
       if($compleanno->format('d') == date('d') && $compleanno->format('m') == date('m')) {
-        $auguri = '<p class="addedProduct">Tanti auguri di buon compleanno da Erboristeria Alchimia, '.$_SESSION['nome_utente'].'! <br/>
-          Per la giornata di oggi, hai diritto ad uno sconto di 10&euro; su un prodotto a tua scelta: corri in negozio, ti aspettiamo. <br/>
-          Ci teniamo a farti gli auguri di persona!
-          </p>';
+        $auguri = '<p class="addedProduct">
+                     Tanti auguri di buon compleanno da Erboristeria Alchimia, '.$_SESSION['nome_utente'].'! <br/>
+                     Per la giornata di oggi, hai diritto ad uno sconto di 10&euro; su un prodotto a tua scelta: corri in negozio, ti aspettiamo. <br/>
+                     Ci teniamo a farti gli auguri di persona!
+                   </p>';
       }
     } // end if $_SESSION['tipo_utente'] == 'User'
+
     $minPrezzoTimbro = 10; // prezzo acquisto che dà diritto ad un timbro
     if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])) {
       $_SESSION["shopping_cart"] = null; // svuoto il carrello
       $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
       if($_SESSION['valAcquisto'] % $minPrezzoTimbro > 0) {
         $num_timbri['numero_timbri'] += $_SESSION['valAcquisto'] % $minPrezzoTimbro;
-        $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto! Ti sono state riempite delle caselle
-        nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio
-        per sfruttarla come buono da 15&euro;.</p>';
+        $aggTimbri = '<p class="addedProduct">
+                        Grazie per il tuo acquisto! Ti sono state riempite delle caselle nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio per sfruttarla come buono da 15&euro;.
+                      </p>';
       }
     }
 
