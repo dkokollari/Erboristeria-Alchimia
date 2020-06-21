493936d05c9795286b9a3d29e7ee86be7f392485
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 18:12:21 +0200

    Aggiunto riferimenti, indentato il codice

--- website/www/php/registrazione.php
+++ website/www/php/registrazione.php
@@ -1,73 +1,78 @@
 <?php
-    require_once("DBAccess.php");
-    require_once("validate_form.php");
+  require_once("genera_pagina.php");
+  require_once("DBAccess.php");
+  require_once("validate_form.php");
 
-    if($_POST['Registrati']) {
-      $nome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['nome']))));
-      $cognome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['cognome']))));
-      $email = mysql_real_escape_string(trim($_POST['username']));
-      $password = mysql_real_escape_string(trim($_POST['password']));
-      $password_conferma = mysql_real_escape_string(trim($_POST['password_conferma']));
-      $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
-      // messaggi di errore
-      $errore_empty = '<span class="errore">Completa tutti i campi</span>';
-      $errore_full = '<span class="errore">Questa e-mail sembra non essere disponibile</span>';
-      $errore_nome = '<span class="errore">Inserisci un nome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
-      $errore_cognome = '<span class="errore">Inserisci un cognome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
-      $errore_email = '<span class="errore">Inserisci una email valida</span>';
-      $errore_password = '<span class="errore">Inserisci una password di lunghezza tra 8 e 100 caratteri, almeno 1 lettera ed 1 numero</span>'; // riferirsi alle regole di validate_form
-      $errore_conferma = '<span class="errore">Le password inserite non corrispondono</span>';
+  if($_POST['Registrati']) {
+    $nome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['nome']))));
+    $cognome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['cognome']))));
+    $email = mysql_real_escape_string(trim($_POST['username']));
+    $password = mysql_real_escape_string(trim($_POST['password']));
+    $password_conferma = mysql_real_escape_string(trim($_POST['password_conferma']));
+    $data_nascita = mysql_real_escape_string(trim($_POST['data_nascita']));
+    // messaggi di errore
+    $errore_empty = '<span class="errore">Completa tutti i campi</span>';
+    $errore_full = '<span class="errore">Questa e-mail sembra non essere disponibile</span>';
+    $errore_nome = '<span class="errore">Inserisci un nome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+    $errore_cognome = '<span class="errore">Inserisci un cognome di lunghezza tra 3 e 100 caratteri</span>'; // riferirsi alle regole di validate_form
+    $errore_email = '<span class="errore">Inserisci una email valida</span>';
+    $errore_password = '<span class="errore">Inserisci una password di lunghezza tra 8 e 100 caratteri, almeno 1 lettera ed 1 numero</span>'; // riferirsi alle regole di validate_form
+    $errore_conferma = '<span class="errore">Le password inserite non corrispondono</span>';
 
-      $fields = [$nome, $cognome, $email, $password, $data_nascita];
-      if(Validate_form::is_empty($fields)) {
-        $errore = $errore_empty;
+    $fields = [$nome, $cognome, $email, $password, $data_nascita];
+    if(Validate_form::is_empty($fields)) {
+      $errore = $errore_empty;
+    }
+    else if(!Validate_form::check_str($nome)) {
+      $errore = $errore_nome;
+    }
+    else if(!Validate_form::check_str($cognome)) {
+      $errore = $errore_cognome;
+    }
+    else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $errore = $errore_email;
+    }
+    else if(!Validate_form::check_pwd($password)) {
+      $errore = $errore_password;
+    }
+    else if($password != $password_conferma) {
+      $errore = $errore_conferma;
+    }
+    else {
+      $con = new DBAccess();
+      if(!$con->openConnection()){
+        header('Location: redirect.php?error=1');
+        exit;
       }
-      else if(!Validate_form::check_str($nome)) {
-        $errore = $errore_nome;
-      }
-      else if(!Validate_form::check_str($cognome)) {
-        $errore = $errore_cognome;
-      }
-      else if(!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errore = $errore_email;
-      }
-      else if(!Validate_form::check_pwd($password)) {
-        $errore = $errore_password;
-      }
-      else if($password != $password_conferma) {
-        $errore = $errore_conferma;
+
+      if($con->getSingolo_Utenti($email)) {
+        $errore = $errore_full;
       }
       else {
-        $con = new DBAccess();
-        if(!$con->openConnection()){
-          header('Location: redirect.php?error=1');
-          exit;
-        }
-
-        if($con->getSingolo_Utenti($email)) {
-          $errore = $errore_full;
-        }
-        else {
-          $con->insertUtenti($nome,
-                             $cognome,
-                             $email,
-                             $password,
-                             $data_nascita);
-        }
-
-        $con->closeConnection();
+        $con->insertUtenti($nome,
+                           $cognome,
+                           $email,
+                           $password,
+                           $data_nascita);
       }
 
-      $status = (empty($errore)
-                ? "<span>Registrazione riuscita</span>"
-                : "<span>Registrazione fallita</span>");
-    } // end if $_POST['Registrati']
-    $pagina = file_get_contents("../html/register.html");
-    $pagina = str_replace("%VALUE_nome%", "", $pagina);
-    $pagina = str_replace("%VALUE_cognome%", "", $pagina);
-    $pagina = str_replace("%VALUE_username%", "", $pagina);
-    $pagina = str_replace("%VALUE_data_nascita%", "", $pagina);
-    $pagina = str_replace("%REGISTER_STATUS%", $status, $pagina);
-    $pagina = str_replace("%REGISTER_ERROR%", $errore, $pagina);
-    echo $pagina;
+      $con->closeConnection();
+    }
+
+    $status = (empty($errore)
+              ? "<span>Registrazione riuscita</span>"
+              : $errore);
+  } // end if $_POST['Registrati']
+
+  $contenuto = file_get_contents("../html/form_utente.html");
+  $contenuto = str_replace("%ACTION_FORM%", "registrazione.php", $contenuto);
+  $contenuto = str_replace("%STATO_UTENTE%", $status, $contenuto);
+  $contenuto = str_replace("%VALUE_nome%", "", $contenuto);
+  $contenuto = str_replace("%VALUE_cognome%", "", $contenuto);
+  $contenuto = str_replace("%VALUE_email%", "", $contenuto);
+  $contenuto = str_replace("%VALUE_data_nascita%", "", $contenuto);
+  $contenuto = str_replace("%NOME_SUBMIT%", "Registrati", $contenuto);
+  $contenuto = str_replace("%AGGIUNGI_SUBMIT%", "", $contenuto);
+  $pagina = Genera_pagina::genera("../html/base5.html", "registrazione", $contenuto);
+  echo $pagina;
 ?>
