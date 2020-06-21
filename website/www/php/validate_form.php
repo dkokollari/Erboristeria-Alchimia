a458f90fdf5bc98018c0362c6992f8306c0f4096
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 21:42:06 +0200

    Modificato commenti

--- website/www/php/validate_form.php
+++ website/www/php/validate_form.php
@@ -1,10 +1,10 @@
 <?php
   class Validate_form {
-    // lunghezza minima 8 e massima 100 caratteri, almeno 1 lettera ed 1 numero
+    // lunghezza minima 8 caratteri e massima 100, almeno 1 lettera ed 1 numero
     public static function check_pwd($password) {
       return preg_match('/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,100}$/', $password);
     }
-    // lunghezza minima 3 e massima 100 caratteri
+    // lunghezza minima 3 caratteri e massima 100
     public static function check_str($stringa) {
       return preg_match('/^([A-Z]|[a-z]){3,100}$/', $stringa);
     }
