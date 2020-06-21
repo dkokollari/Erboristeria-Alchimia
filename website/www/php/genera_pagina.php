cf15b9e57e5ba8ee6e993c42e82821b275808d71
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 17:39:36 +0200

    Rinominato placeholder dentro le basi

--- website/www/php/genera_pagina.php
+++ website/www/php/genera_pagina.php
@@ -7,9 +7,12 @@
     public function genera($base, $target, $contenuto="") {
       $pagina = file_get_contents($base);
       // impostazione icona del carrello
-      if($_SESSION['auth'] && $_SESSION['tipo_utente']=="User") {
-        $icona_carrello = '<span id="cart_icon" class="material-icons-outlined top_icon">shopping_cart</span>';
+      if($_SESSION['auth']) {
+        if($_SESSION['tipo_utente']=="User") {
+          $icona_top = '<span id="cart_icon" class="material-icons-outlined top_icon">shopping_cart</span>';
+        }
       }
+
       // pagine generabili
       switch($target) {
         case "carrello" :
@@ -17,10 +20,10 @@
           $titolo_pagina = "Carrello di Erboristeria Alchimia";
           $descrizione_pagina = "Il tuo carrello verde made by Erboristeria Alchimia";
           $keywords_pagina = "carrello, acquista, acquisti, erboristeria, alchimia";
-          $aggiungi_link = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>';
+          $meta_link = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>';
           $container_pagina = "container_prodotti"; // TODO: controllare  effetti su CSS usando container_carrello
           $attributi_body = 'id="carrello"';
-          $icona_carello = "";
+          $icona_top = "";
           $lista_menu = menu_pagina::menu($target);
         break;
 
@@ -121,15 +124,15 @@
       $pagina = str_replace("%TITOLO_PAGINA%", $titolo_pagina, $pagina);
       $pagina = str_replace("%DESCRIZIONE_PAGINA%", $descrizione_pagina, $pagina);
       $pagina = str_replace("%KEYWORDS_PAGINA%", $keywords_pagina, $pagina);
-      $pagina = str_replace("%AGGIUNGI_META%", $aggiungi_meta, $pagina);
-      $pagina = str_replace("%AGGIUNGI_LINK%", $aggiungi_link, $pagina);
-      $pagina = str_replace("%AGGIUNGI_SCRIPT%", $aggiungi_script, $pagina);
+      $pagina = str_replace("%META_HEAD%", $meta_head, $pagina);
+      $pagina = str_replace("%LINK_HEAD%", $meta_link, $pagina);
+      $pagina = str_replace("%SCRIPT_HEAD%", $meta_script, $pagina);
       $pagina = str_replace("%ATTRIBUTI_BODY%", $attributi_body, $pagina);
       $pagina = str_replace("%CONTAINER_PAGINA%", $container_pagina, $pagina);
-      $pagina = str_replace("%ICONA_CARRELLO%", $icona_carrello, $pagina);
+      $pagina = str_replace("%ICONA_TOP%", $icona_top, $pagina);
       $pagina = str_replace("%LISTA_MENU%", $lista_menu, $pagina);
       $pagina = str_replace("%CONTENUTO_PAGINA%", $contenuto, $pagina);
-      $pagina = str_replace("%SCRIPT_BODY%", $body_script, $pagina);
+      $pagina = str_replace("%SCRIPT_BODY%", $script_body, $pagina);
       return $pagina;
     }
 
