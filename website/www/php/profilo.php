5496778bac70968a3eca146f97d523f4469aaff9
Fouad <fouadmouad5@gmail.com>
2020-06-21 10:42:17 +0200

    Scritto messaggio ringraziamento per l'acquisto

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -18,12 +18,17 @@
       }
     }
     $con->closeConnection();
+    $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
     $minPrezzoTimbro = 10; //prezzo acquisto che dà diritto ad un timbro
     if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])) {
       $num_timbri['numero_timbri'] += $_SESSION['valAcquisto'] % $minPrezzoTimbro;
+      $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto! Ti sono stati convalidate delle caselle
+       nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio
+       per sfruttarla come buono da 15&euro;.</p>';
     }
 
     $contenuto = file_get_contents("../html/profilo.html");
+    $contenuto = str_replace("%AGG_TIMBRI%", $aggTimbri, $contenuto);
     $contenuto = str_replace("%TIMBRI%", $img_timbri, $contenuto);
     $contenuto = str_replace("%NUMERO_TIMBRI%", $num_timbri['numero_timbri'], $contenuto);
     $pagina = Genera_pagina::genera("../html/base.html", "profilo", $contenuto);
