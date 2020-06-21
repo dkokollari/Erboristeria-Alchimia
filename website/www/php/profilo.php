c884e0cc9075c778d4e31bd72b9906b156e226d3
Fouad <fouadmouad5@gmail.com>
2020-06-21 15:22:55 +0200

    Fix

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -11,28 +11,39 @@
     }
     $img_timbri = '';
     $num_timbri = 0;
+    $auguri = '';
     if($_SESSION['tipo_utente'] == 'User') {
       $num_timbri = $con->getTimbriUtente($_SESSION['email_utente'])[0]['numero_timbri'];
       for($i=0; $i<$num_timbri; ++$i) {
         $img_timbri .= '<img id="#timbro_' . ($i+1) . '" src="../img/carta_fedelta/2.png"/>' . "\n";
       }
+      $compleanno = DateTime::createFromFormat("Y-m-d", $_SESSION['data_nascita_utente']);
+      if($compleanno->format('d') == date('d') && $compleanno->format('m') == date('m')) {
+        $auguri = '<p class="addedProduct">Tanti auguri di buon compleanno da Erboristeria Alchimia, ' .
+        $_SESSION['nome_utente'] .' ! Per la giornata di oggi, hai diritto ad uno sconto di 10&euro;
+         su un prodotto a tua scelta: corri in negozio, ti aspettiamo; ci teniamo a farti gli auguri
+         di persona ! </p>';
+      }
     }
     $con->closeConnection();
-    $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
+    $aggTimbri = '';
     $minPrezzoTimbro = 10; //prezzo acquisto che dà diritto ad un timbro
-    if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])
-        && $_SESSION['valAcquisto'] % $minPrezzoTimbro > 0) {
-      $_SESSION["shopping_cart"] = null; //svuoto il carrello    
+    if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])) {
+      $_SESSION["shopping_cart"] = null; //svuoto il carrello
+      $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
+    if($_SESSION['valAcquisto'] % $minPrezzoTimbro > 0) {
       $num_timbri['numero_timbri'] += $_SESSION['valAcquisto'] % $minPrezzoTimbro;
       $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto! Ti sono stati convalidate delle caselle
        nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio
        per sfruttarla come buono da 15&euro;.</p>';
+     }
     }
 
     $contenuto = file_get_contents("../html/profilo.html");
     $contenuto = str_replace("%AGG_TIMBRI%", $aggTimbri, $contenuto);
     $contenuto = str_replace("%TIMBRI%", $img_timbri, $contenuto);
     $contenuto = str_replace("%NUMERO_TIMBRI%", $num_timbri['numero_timbri'], $contenuto);
+    $contenuto = str_replace("%AUGURI%", $auguri, $contenuto);
     $pagina = Genera_pagina::genera("../html/base.html", "profilo", $contenuto);
     echo $pagina;
   } else {
