e397578b7e9e20a870a5062ed8a0498a4d606179
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-21 22:27:10 +0200

    Spostato prelievo tessera dopo eventuale modifica/cancellazione profilo

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -5,40 +5,6 @@
   require_once("validate_form.php");
 
   if($_SESSION['auth']) {
-    $con = new DBAccess();
-    if(!$con->openConnection()) {
-      header('Location: redirect.php?error=1');
-      exit;
-    }
-    $num_timbri = 0;
-    if($_SESSION['tipo_utente'] == 'User') {
-      $num_timbri = $con->getTimbriUtente($_SESSION['email_utente'])[0]['numero_timbri'];
-      $con->closeConnection();
-      for($i = 0; $i < $num_timbri; $i++) {
-        $img_timbri .= '<img id="#timbro_'.($i+1).'" src="../img/carta_fedelta/2.png"/>'."\n";
-      }
-      $compleanno = DateTime::createFromFormat("Y-m-d", $_SESSION['data_nascita_utente']);
-      if($compleanno->format('d') == date('d') && $compleanno->format('m') == date('m')) {
-        $auguri = '<p class="addedProduct">
-                     Tanti auguri di buon compleanno da Erboristeria Alchimia, '.$_SESSION['nome_utente'].'! <br/>
-                     Per la giornata di oggi, hai diritto ad uno sconto di 10&euro; su un prodotto a tua scelta: corri in negozio, ti aspettiamo. <br/>
-                     Ci teniamo a farti gli auguri di persona!
-                   </p>';
-      }
-    } // end if $_SESSION['tipo_utente'] == 'User'
-
-    $minPrezzoTimbro = 10; // prezzo acquisto che dà diritto ad un timbro
-    if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])) {
-      $_SESSION["shopping_cart"] = null; // svuoto il carrello
-      $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
-      if($_SESSION['valAcquisto'] % $minPrezzoTimbro > 0) {
-        $num_timbri['numero_timbri'] += $_SESSION['valAcquisto'] % $minPrezzoTimbro;
-        $aggTimbri = '<p class="addedProduct">
-                        Grazie per il tuo acquisto! Ti sono state riempite delle caselle nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio per sfruttarla come buono da 15&euro;.
-                      </p>';
-      }
-    }
-
     if($_POST['Modifica_profilo']) {
       $nome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['nome']))));
       $cognome = ucfirst(strtolower(mysql_real_escape_string(trim($_POST['cognome']))));
@@ -106,6 +72,41 @@
                 ? "<span>Profilo aggiornato con successo</span>"
                 : $errore);
     } // end if $_POST['Modifica_profilo']
+
+    // prelievo tessera utente e visualizzazione messaggi
+    $con = new DBAccess();
+    if(!$con->openConnection()) {
+      header('Location: redirect.php?error=1');
+      exit;
+    }
+    $num_timbri = 0;
+    if($_SESSION['tipo_utente'] == 'User') {
+      $num_timbri = $con->getTimbriUtente($_SESSION['email_utente'])[0]['numero_timbri'];
+      $con->closeConnection();
+      for($i = 0; $i < $num_timbri; $i++) {
+        $img_timbri .= '<img id="#timbro_'.($i+1).'" src="../img/carta_fedelta/2.png"/>'."\n";
+      }
+      $compleanno = DateTime::createFromFormat("Y-m-d", $_SESSION['data_nascita_utente']);
+      if($compleanno->format('d') == date('d') && $compleanno->format('m') == date('m')) {
+        $auguri = '<p class="addedProduct">
+                     Tanti auguri di buon compleanno da Erboristeria Alchimia, '.$_SESSION['nome_utente'].'! <br/>
+                     Per la giornata di oggi, hai diritto ad uno sconto di 10&euro; su un prodotto a tua scelta: corri in negozio, ti aspettiamo. <br/>
+                     Ci teniamo a farti gli auguri di persona!
+                   </p>';
+      }
+    } // end if $_SESSION['tipo_utente'] == 'User'
+
+    $minPrezzoTimbro = 10; // prezzo acquisto che dà diritto ad un timbro
+    if(isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto'])) {
+      $_SESSION["shopping_cart"] = null; // svuoto il carrello
+      $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
+      if($_SESSION['valAcquisto'] % $minPrezzoTimbro > 0) {
+        $num_timbri['numero_timbri'] += $_SESSION['valAcquisto'] % $minPrezzoTimbro;
+        $aggTimbri = '<p class="addedProduct">
+                        Grazie per il tuo acquisto! Ti sono state riempite delle caselle nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio per sfruttarla come buono da 15&euro;.
+                      </p>';
+      }
+    }
 
     $contenuto = file_get_contents("../html/profilo.html");
     $contenuto = str_replace("%AGG_TIMBRI%", $aggTimbri, $contenuto);
