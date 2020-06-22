be6822482f428190f24b84d81c1d30600c1927bf
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-22 12:32:52 +0200

    Commentato temporaneamente controlli accesso

--- website/www/php/admin.php
+++ website/www/php/admin.php
@@ -2,14 +2,14 @@
   require_once("session.php");
   require_once("genera_pagina.php");
 
-  if($_SESSION['auth'] && $_SESSION['tipo_utente'] == "Admin") {
+  // if($_SESSION['auth'] && $_SESSION['tipo_utente'] == "Admin") {
 
     $contenuto = file_get_contents("../html/admin.html");
     $pagina = Genera_pagina::genera("../html/base5.html", "admin", $contenuto);
     echo $pagina;
-  } // end if $_SESSION['auth'] && $_SESSION['tipo_utente'] == "Admin"
-  else {
-    header('Location: redirect.php?error=3');
-    exit;
-  }
+  // } // end if $_SESSION['auth'] && $_SESSION['tipo_utente'] == "Admin"
+  // else {
+  //   header('Location: redirect.php?error=3');
+  //   exit;
+  // }
 ?>
