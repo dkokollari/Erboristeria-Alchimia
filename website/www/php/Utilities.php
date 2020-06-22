47c15c03f82055791c3ae27e92baf07088fbce36
dkokollari <38751431+dkokollari@users.noreply.github.com>
2020-06-22 13:21:59 +0200

    Aggiunto getNumeric_Value_POST()

--- website/www/php/Utilities.php
+++ website/www/php/Utilities.php
@@ -1,9 +1,15 @@
 <?php
   class Utilities {
 
-    public static function getNumericValue($index) {
+    public static function getNumericValue_GET($index) {
       return ((isset($_GET[$index]) && is_numeric($_GET[$index]) && (int)($_GET[$index]) > 0)
               ? (int)$_GET[$index]
+              : 1);
+    }
+
+    public static function getNumericValue_POST($index) {
+      return ((isset($_POST[$index]) && is_numeric($_POST[$index]) && (int)($_POST[$index]) > 0)
+              ? (int)$_POST[$index]
               : 1);
     }
 
