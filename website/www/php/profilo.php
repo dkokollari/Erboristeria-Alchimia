42f1fc4484ec8f58213c1bca176d7aaa4be7181d
Fouad <fouadmouad5@gmail.com>
2020-06-25 19:16:50 +0200

    Gaggi infame, per te solo le lame

--- website/www/php/profilo.php
+++ website/www/php/profilo.php
@@ -59,7 +59,7 @@ if ($_SESSION['auth'])
       }
     }
 
-    $status = (empty($errore) ? "<span class=\"addedProduct\">Profilo aggiornato con successo</span>" : $errore);
+    $status = (empty($errore) ? "<span class=\"success\">Profilo aggiornato con successo</span>" : $errore);
   } // end if $_POST['Modifica_profilo']
   // prelievo tessera utente e visualizzazione messaggi
   if ($_SESSION['tipo_utente'] == 'User')
@@ -73,7 +73,7 @@ if ($_SESSION['auth'])
     $compleanno = DateTime::createFromFormat("Y-m-d", $_SESSION['data_nascita_utente']);
     if ($compleanno->format('d') == date('d') && $compleanno->format('m') == date('m'))
     {
-      $auguri = '<p class="addedProduct">
+      $auguri = '<p class="success">
                      Tanti auguri di buon compleanno da Erboristeria Alchimia, ' . $_SESSION['nome_utente'] . '! <br/>
                      Per la giornata di oggi, hai diritto ad uno sconto di 10&euro; su un prodotto a tua scelta: corri in negozio, ti aspettiamo. <br/>
                      Ci teniamo a farti gli auguri di persona!
@@ -82,11 +82,11 @@ if ($_SESSION['auth'])
     if (isset($_SESSION['valAcquisto']) && !empty($_SESSION['valAcquisto']))
     {
       $_SESSION["shopping_cart"] = null; // svuoto il carrello
-      $aggTimbri = '<p class="addedProduct">Grazie per il tuo acquisto!</p>';
+      $aggTimbri = '<p class="success">Grazie per il tuo acquisto!</p>';
       if ($_SESSION['valAcquisto'] % $minPrezzoTimbro > 0)
       {
         $num_timbri += (int)($_SESSION['valAcquisto'] / $minPrezzoTimbro);
-        $aggTimbri = '<p class="addedProduct">
+        $aggTimbri = '<p class="success">
                           Grazie per il tuo acquisto! Ti sono state riempite delle caselle nella tua carta fedelt&agrave;: quando la tua carta sar&agrave; piena, recati in negozio per sfruttarla come buono da 15&euro;.
                         </p>';
       }
